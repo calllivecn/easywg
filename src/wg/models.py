d271571e3f2c17a419ddfd29b826ea0a078d98f0
calllivecn <c-all@qq.com>
2020-06-12 18:10:21 +0800

    save...

--- src/wg/models.py
+++ src/wg/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ServerWg(models.Model):
+    ifname = models.CharField(max_length=32, unique=True)
+    privatekey = models.CharField(max_length=64)
+    presharedkey = models.CharField(max_length=64)
+    listenport = models.IntegerField(blank=True, null=True)
+    allowedips = models.TextField(null=True)
+
+
+class ClientWg(models.Model):
+    server = models.ForeignKey(ServerWg, on_delete=models.CASCADE)
+    user = alskdfjief()
+    ifname = models.CharField(max_length=32, unique=True)
+    privatekey = models.CharField(max_length=64)
+    presharedkey = models.CharField(max_length=64)
+    listenport = models.IntegerField(blank=True, null=True)
+    allowedips = models.TextField()
