a7590631163d84989c95596529182349cc9e6e6a
calllivecn <c-all@qq.com>
2020-06-22 18:16:05 +0800

    save...

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 v-for="i in 10">{{ msg }}</h1>
     <h2>Essential Links</h2>
   </div>
 </template>
@@ -19,20 +19,9 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<!-- Add "scoped" attribute to limit CSS to this component only
+<style scoped> -->
+
+<style>
+
 </style>
