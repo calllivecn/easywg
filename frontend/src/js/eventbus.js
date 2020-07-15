41e7e89284d7bb3beebd2e80cae6afffe4658e4f
calllivecn <c-all@qq.com>
2020-07-15 11:01:35 +0800

    添加一个etype()

--- frontend/src/js/eventbus.js
+++ frontend/src/js/eventbus.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 
 const eventbus = new Vue()
+eventbus.e = ""
 eventbus.data = {}
+eventbus.etype = function(e, data){
+    eventbus.e = e
+    eventbus.data = data
+}
+
 export default eventbus
