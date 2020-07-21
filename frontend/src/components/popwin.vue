f2c348da21df37fc366491271e9b0c2b7164164b
calllivecn <c-all@qq.com>
2020-07-21 18:03:51 +0800

    写出来了，弹窗，下一步，把二维码，放进去～！

---
+++ frontend/src/components/popwin.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="show popwin" v-show="show">
+        <header>{{ title }}</header>
+        <div v-html="text"></div>
+        <div>
+            <slot></slot>
+        </div>
+        <button @click="ok">关闭</button>
+    </div>
+</template>
+<script>
+import eventbus from '../js/eventbus.js'
+export default {
+    props: ['show', 'title', 'text'],
+    data() {
+        return {
+        }
+    },
+    methods: {
+        ok: function(){
+            eventbus.$emit('popwin', false)
+        },
+    }
+}
+</script>
+
+<style>
+.popwin {
+    background-color: aliceblue;
+    text-align: center;
+    position: fixed;
+    z-index: 1;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+}
+</style>
