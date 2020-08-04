99188000c5e2ffd1f61a245f19ccf0c1cb4857cb
calllivecn <calllivecn@outlook.com>
2020-08-04 16:11:12 +0800

    添加 main.py --debug --export-staticfiles 选项 vue-router mode: history

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,11 +4,12 @@ import Router from 'vue-router'
 import home from '@/components/home'
 import login from '@/components/login'
 import chpassword from '@/components/chpassword'
+import notfound from '@/components/notfound'
 
 Vue.use(Router)
 
 export default new Router({
-    //mode: "history",
+    mode: "history",
     routes: [
         {
             path: "/",
@@ -28,6 +29,10 @@ export default new Router({
             path: "/chpassword",
             name: "chpassword",
             component: chpassword
+        },
+        {
+            path: '*',
+            component: notfound
         }
     ]
 })
