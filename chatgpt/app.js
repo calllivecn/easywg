96d263351a2d8f6b8a7bfa70be4fa63f8b521671
calllivecn <calllivecn@outlook.com>
2023-04-16 10:38:17 +0800

    test v2

---
+++ chatgpt/app.js
@@ -0,0 +1,55 @@
+const configListEl = document.querySelector('.config-list')
+const addConfigBtnEl = document.getElementById('add-config-btn')
+const deleteConfigBtnEl = document.getElementById('delete-config-btn')
+
+let configs = []
+let activeConfig = null
+
+function updateConfigList() {
+  configListEl.innerHTML = ''
+  for (const config of configs) {
+    const configItemEl = document.createElement('div')
+    configItemEl.classList.add('config-item')
+    configItemEl.textContent = config
+    if (config === activeConfig) {
+      configItemEl.classList.add('active')
+    }
+    configItemEl.addEventListener('click', () => {
+      setActiveConfig(config)
+    })
+    configListEl.appendChild(configItemEl)}
+}
+
+function addConfig() {
+const name = prompt('Enter the name of the new configuration:')
+if (name && !configs.includes(name)) {
+configs.push(name)
+updateConfigList()
+}
+}
+
+function deleteConfig() {
+if (activeConfig) {
+const index = configs.indexOf(activeConfig)
+if (index !== -1) {
+configs.splice(index, 1)
+setActiveConfig(null)
+updateConfigList()
+}
+}
+}
+
+function setActiveConfig(config) {
+activeConfig = config
+updateConfigList()
+}
+
+addConfigBtnEl.addEventListener('click', addConfig)
+deleteConfigBtnEl.addEventListener('click', deleteConfig)
+
+// Initialize the UI
+configs = ['config1', 'config2'] // Replace with actual configs
+activeConfig = configs[0]
+updateConfigList()
+
+
