70ba4a526346ba6562ed8f028cf3edd7014b740f
calllivecn <c-all@qq.com>
2023-06-22 22:03:42 +0800

    save

--- src/config.py
+++ src/config.py
@@ -8,28 +8,28 @@ import json
 
 
 wgconf = {
-        "id": 1,
-        "interface":
+    "id": 1,
+    "interface":
+    {
+        "ifname": "<15byte>",
+        "privkey": "<privkey>",
+        "address": "easywg.example.com",
+        "ip": "10.1.1.1/24",
+        "listenport": "可选",
+        "allowedips": ["10.1.1.0/24", "192.168.1.0/24"],
+        "DNS": ["223.5.5.5", "ipv6"],
+        "comment": "描述 可选",
+    },
+    "peers": [
         {
-                "ifname": "<15byte>",
-                "privkey": "<privkey>",
-                "address": "easywg.example.com",
-                "ip": "10.1.1.1/24",
-                "listenport": "可选",
-                "allowedips": ["10.1.1.0/24", "192.168.1.0/24"],
-                "DNS": ["223.5.5.5", "ipv6"],
-                "comment": "描述 可选",
-        },
-        "peers": [
-                {
-                        "ifname": "<15byte>",
-                        "pubkey": "<pubkey",
-                        "pskkey": "[pskkey]",
-                        "address": "easywg.example.com",
-                        "ip": "10.1.1.1/24",
-                        "listenport": "可选",
-                        "endpoint": "donamename.com",
-                        "allowedips": ["10.1.1.10/32", "192.168.1.0/24"],
-                }
-        ]
+            "ifname": "<15byte>",
+            "pubkey": "<pubkey",
+            "pskkey": "[pskkey]",
+            "address": "easywg.example.com",
+            "ip": "10.1.1.1/24",
+            "listenport": "可选",
+            "endpoint": "donamename.com",
+            "allowedips": ["10.1.1.10/32", "192.168.1.0/24"],
+        }
+    ]
 }
