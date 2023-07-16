5767472c8d551230144e28f409bbf721363e07ae
calllivecn <c-all@qq.com>
2023-07-16 12:22:14 +0800

    修复，每次都要修改整个peer端。

--- wg-ddns/client.py
+++ wg-ddns/client.py
@@ -27,7 +27,7 @@ def getlogger(level=logging.INFO):
     logger.setLevel(level)
     return logger
 
-logger = getlogger()
+logger = getlogger(logging.DEBUG)
 
 
 # 加载配置文件
@@ -49,6 +49,8 @@ def check_alive(server_wg_ip):
             if failed_count >= 3:
                 logger.warning(f"{server_wg_ip} 线路断开了...")
                 return
+            else:
+                continue
 
         if failed_count > 0:
             logger.info(f"{server_wg_ip} 检测恢复...")
@@ -70,40 +72,50 @@ def main():
     wg_name = ifname["interface"]
     util.ip_link_add_wg(wg_name)
 
+
     atexit.register(lambda :util.ip_link_del_wg(wg_name))
     
     for CIDR in ifname["address"]:
         util.ip_addr_add(wg_name, CIDR)
 
     util.wg_set(wg_name, ifname["private_key"], listen_port=ifname.get("listen_port"), fwmark=ifname.get("fwmark"))
+
     for peer in conf["peers"]:
         util.wg_peer(wg_name, peer)
     
 
     while True:
         server_addr = conf["peers"][0]["endpoint_addr"]
+        public_key = conf["peers"][0]["public_key"]
 
         check_alive(conf["server_wg_ip"])
 
         logger.info(f"重新解析域名，并更新wireguard。")
 
+        logger.debug(f"{server_addr=} {public_key=}")
+
+        util.wg_peer(wg_name, conf["peers"][0])
+        
+        """
         # 需要更新域名指向
         ipv4, ipv6 = util.get_ip_by_addr(server_addr)
+        logger.debug(f"{ipv4=} {ipv6=}")
 
         # 缓存下，方便检测，域名有没有更新, 不用，压力给到 check_alive()
 
         # 优先使用ipv6
         if len(ipv6) > 0:
-            logger.info(f"使用域名：{ipv6[0]}")
-            util.wg_peer_option("endpoint_addr", ipv6[0])
+            logger.info(f"使用新地址：{ipv6[0]}")
+            util.wg_peer_option(wg_name, public_key, {"endpoint_addr": ipv6[0]})
 
         elif len(ipv4) > 0:
-            logger.info(f"使用域名：{ipv4[0]}")
-            util.wg_peer_option("endpoint_addr", ipv4[0])
+            logger.info(f"使用地址：{ipv4[0]}")
+            util.wg_peer_option(wg_name, public_key, {"endpoint_addr": ipv4[0]})
         else:
-            logger.waring("没有解析到域名！")
+            logger.warning("没有解析到地址！")
 
+        """
 
 if __name__ == "__main__":            
-    logger.setLevel(logging.DEBUG)
+    # 怎么没用 ？ logger.setLevel(logging.DEBUG)
     main()
