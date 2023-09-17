7f3184230d1e633ff56558e70b800537cdad20b9
calllivecn <c-all@qq.com>
2023-09-17 22:16:45 +0800

    test

--- wg-ddns/src/test.py
+++ wg-ddns/src/test.py
@@ -12,15 +12,79 @@ from pyroute2 import (
 ndb = NDB()
 
 def list_wg():
-    r = ndb.interfaces.summary()
-    # r = ndb.interfaces.dump()
-    r.filter(kind="wireguard")
-    r.select("index", "ifname", "kind")
-    r.format("json")
+    r = (
+        ndb.interfaces.summary()
+        # ndb.interfaces.dump()
+        .filter(kind="wireguard")
+        .select("index", "ifname", "kind")
+        .format("json")
+    )
     return r
 
 
+import pprint
+
+def show_wg(ifname):
+    """
+    wg:
+    {'attrs': [('WGDEVICE_A_LISTEN_PORT', 18000),
+           ('WGDEVICE_A_FWMARK', 0),
+           ('WGDEVICE_A_IFINDEX', 6),
+           ('WGDEVICE_A_IFNAME', 'wg-pyz'),
+           ('WGDEVICE_A_PRIVATE_KEY', b'SGjim+kz/TYafPCp8i5s2ga5HNl/lRE8L1rYL4SNcXk='),
+           ('WGDEVICE_A_PUBLIC_KEY', b'vylLRq36NG76807VDAhmFrJfcanlGTIVIVXCZqNa5nY='),
+           ('WGDEVICE_A_PEERS', [{'attrs': [('WGPEER_A_PUBLIC_KEY', b'aEhoRpWJiJGAMD4MLw0WpmWcsLrUUGArb7rzuBITOiw='), ('WGPEER_A_PRESHARED_KEY', b'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA='), ('WGPEER_A_LAST_HANDSHAKE_TIME', {'tv_sec': 1694717024, 'tv_nsec': 765360652, 'latest handshake': 'Fri Sep 15 02:43:44 2023'}), ('WGPEER_A_PERSISTENT_KEEPALIVE_INTERVAL', 0), ('WGPEER_A_TX_BYTES', 167968360), ('WGPEER_A_RX_BYTES', 183858848), ('WGPEER_A_PROTOCOL_VERSION', 1), ('WGPEER_A_ENDPOINT', {'family': 10, 'port': 18000, 'flowinfo': 0, 'addr': '240e:3ba:30b1:4b40:e4d2:649f:e95b:bd39', 'scope_id': 0}), ('WGPEER_A_ALLOWEDIPS', [{'attrs': [('WGALLOWEDIP_A_CIDR_MASK', 24), ('WGALLOWEDIP_A_FAMILY', 2), ('WGALLOWEDIP_A_IPADDR', '0a:01:03:00')], 'addr': '10.1.3.0/24'}, {'attrs': [('WGALLOWEDIP_A_CIDR_MASK', 64), ('WGALLOWEDIP_A_FAMILY', 10), ('WGALLOWEDIP_A_IPADDR', 'fc:03:00:00:00:00:00:00:00:00:00:00:00:00:00:00')], 'addr': 'fc03::/64'}], 32768)]}], 32768)],
+    'cmd': 0,
+    'header': {'error': None,
+               'flags': 2,
+               'length': 372,
+               'pid': 2728308,
+               'sequence_number': 255,
+               'stats': Stats(qsize=0, delta=0, delay=0),
+               'target': 'localhost',
+               'type': 35},
+    'reserved': 0,
+    'version': 1}
+    """
+
+    with WireGuard() as wg:
+        wg = wg.info(ifname)[0]
+    
+    return wg
+
+    # wg 从pyroute2 里 解析 出来 dict 的 信息格式
+    # 之后还需要添加，address, 
+    wg_conf = {}
+    for k, v in wg["attrs"]:
+        if "IFNAME" in k:
+            wg_conf["interface"] = v
+
+        elif "PRIVATE_KEY" in k:
+            wg_conf["private_key"] = v
+
+        elif "PUBLIC_KEY" in k:
+            wg_conf["public_key"] = v
+        
+        elif "LISTEN_PORT" in k:
+            wg_conf["listen_port"] = v
+        
+        else:
+            print(f"跳过: {k=} {v=}")
+
+
+    peers_conf = []
+    wg_conf["peers"] = peers_conf
+    peers = wg["attrs"]["WGDEVICE_A_PEERS"]
+    for p in peers:
+        if "PUBLIC_KEY" in p[0]:
+            peers_conf.append()
+
+
+    return wg
+
 
 print(list_wg())
+wg = show_wg("wg-pyz")
+print(pprint.pformat(wg))
 
 ndb.close()
