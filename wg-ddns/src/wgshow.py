19b77110800dcdf1d6af0c68766f4b88e9d3f331
calllivecn <c-all@qq.com>
2023-11-17 00:31:12 +0800

    save

--- wg-ddns/src/wgshow.py
+++ wg-ddns/src/wgshow.py
@@ -5,6 +5,7 @@
 
 
 import json
+import copy
 
 
 from pyroute2 import (
@@ -52,14 +53,14 @@ class WgShow:
 
     def show_wg(self, ifname):
         """
-        wg:
+        wg show:
         {'attrs': [('WGDEVICE_A_LISTEN_PORT', 18000),
                ('WGDEVICE_A_FWMARK', 0),
                ('WGDEVICE_A_IFINDEX', 6),
                ('WGDEVICE_A_IFNAME', 'wg-pyz'),
-               ('WGDEVICE_A_PRIVATE_KEY', b'SGjim+kz/TYafPCp8i5s2ga5HNl/lRE8L1rYL4SNcXk='),
-               ('WGDEVICE_A_PUBLIC_KEY', b'vylLRq36NG76807VDAhmFrJfcanlGTIVIVXCZqNa5nY='),
-               ('WGDEVICE_A_PEERS', [{'attrs': [('WGPEER_A_PUBLIC_KEY', b'aEhoRpWJiJGAMD4MLw0WpmWcsLrUUGArb7rzuBITOiw='), ('WGPEER_A_PRESHARED_KEY', b'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA='), ('WGPEER_A_LAST_HANDSHAKE_TIME', {'tv_sec': 1694717024, 'tv_nsec': 765360652, 'latest handshake': 'Fri Sep 15 02:43:44 2023'}), ('WGPEER_A_PERSISTENT_KEEPALIVE_INTERVAL', 0), ('WGPEER_A_TX_BYTES', 167968360), ('WGPEER_A_RX_BYTES', 183858848), ('WGPEER_A_PROTOCOL_VERSION', 1), ('WGPEER_A_ENDPOINT', {'family': 10, 'port': 18000, 'flowinfo': 0, 'addr': '240e:3ba:30b1:4b40:e4d2:649f:e95b:bd39', 'scope_id': 0}), ('WGPEER_A_ALLOWEDIPS', [{'attrs': [('WGALLOWEDIP_A_CIDR_MASK', 24), ('WGALLOWEDIP_A_FAMILY', 2), ('WGALLOWEDIP_A_IPADDR', '0a:01:03:00')], 'addr': '10.1.3.0/24'}, {'attrs': [('WGALLOWEDIP_A_CIDR_MASK', 64), ('WGALLOWEDIP_A_FAMILY', 10), ('WGALLOWEDIP_A_IPADDR', 'fc:03:00:00:00:00:00:00:00:00:00:00:00:00:00:00')], 'addr': 'fc03::/64'}], 32768)]}], 32768)],
+               ('WGDEVICE_A_PRIVATE_KEY', b'000000000000000000000000000000'),
+               ('WGDEVICE_A_PUBLIC_KEY', b'000000000000000000000000000000'),
+               ('WGDEVICE_A_PEERS', [{'attrs': [('WGPEER_A_PUBLIC_KEY', b'000000000000000000000000000000'), ('WGPEER_A_PRESHARED_KEY', b'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA='), ('WGPEER_A_LAST_HANDSHAKE_TIME', {'tv_sec': 1694717024, 'tv_nsec': 765360652, 'latest handshake': 'Fri Sep 15 02:43:44 2023'}), ('WGPEER_A_PERSISTENT_KEEPALIVE_INTERVAL', 0), ('WGPEER_A_TX_BYTES', 167968360), ('WGPEER_A_RX_BYTES', 183858848), ('WGPEER_A_PROTOCOL_VERSION', 1), ('WGPEER_A_ENDPOINT', {'family': 10, 'port': 18000, 'flowinfo': 0, 'addr': '240e:3ba:30b1:4b40:e4d2:649f:e95b:bd39', 'scope_id': 0}), ('WGPEER_A_ALLOWEDIPS', [{'attrs': [('WGALLOWEDIP_A_CIDR_MASK', 24), ('WGALLOWEDIP_A_FAMILY', 2), ('WGALLOWEDIP_A_IPADDR', '0a:01:03:00')], 'addr': '10.1.3.0/24'}, {'attrs': [('WGALLOWEDIP_A_CIDR_MASK', 64), ('WGALLOWEDIP_A_FAMILY', 10), ('WGALLOWEDIP_A_IPADDR', 'fc:03:00:00:00:00:00:00:00:00:00:00:00:00:00:00')], 'addr': 'fc03::/64'}], 32768)]}], 32768)],
         'cmd': 0,
         'header': {'error': None,
                    'flags': 2,
@@ -71,6 +72,23 @@ class WgShow:
                    'type': 35},
         'reserved': 0,
         'version': 1}
+
+        -----------------
+        wg_conf:[
+            {'address': ['10.x.x.x/24', 'fcxx::xx/64'],
+            'fwmark': 0,
+            'interface': 'wg-pyz',
+            'listen_port': 18000,
+            'peers': [{'allowed_ips': ['10.x.x.x/24', 'fcxx::xx/64'],
+                       'endpoint_addr': 'xxxx:xxxx/64',
+                       'endpoint_port': 18000,
+                       'public_key': b'00000000000000000000000000000000000000',
+                       'rx_bytes': 25328587220,
+                       'tx_bytes': 958578428,
+                       'wg_protocol_version': 1}],
+            'private_key': b'000000000000000000000000000000000000000000',
+            'public_key': b'000000000000000000000000000000000000'}
+        ]
         """
 
         with WireGuard() as wg:
@@ -157,8 +175,26 @@ class WgShow:
 
         wg_conf["peers"] = peers_conf
 
+        self.peers_conf = peer_conf
         return wg_conf
 
+
+    def get_wg_conf(self):
+        """
+        返回可转发的配置信息
+        """
+        confs = copy.deepcopy(self.peers_conf)
+
+        data = []
+        for conf in confs:
+            conf.pop("private_key")
+            conf.pop("")
+
+    
+    def wg_conf_transport(self):
+
+        self.get_wg_conf
+        
 
 
 def main():
