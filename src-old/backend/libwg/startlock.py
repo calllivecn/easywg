a4b58cfb8d94bdce2877297ff4437875d14f470e
calllivecn <calllivecn@outlook.com>
2024-04-01 23:06:36 +0800

    删除没有在使用的

--- src-old/backend/libwg/startlock.py
+++
@@ -1,7 +0,0 @@
-
-from threading import Lock
-
-__all__ = ("START_LOCK")
-
-START_LOCK = Lock()
-START_LOCK.acquire()
--- src-old/frontend/src/js/funcs.js
+++
@@ -1,5 +0,0 @@
-export default {
-    myfunc(){
-        console.log("看到了吗？ 成功自定义函数～！～！～！")
-    },
-}
